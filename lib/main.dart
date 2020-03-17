b15233529f468520a80caf01052bae389942e199
Alish Chakradhar <alishchakradhar@gmail.com>
2020-03-18 09:28:15 +1100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+//The main function is the entrance point for all our flutter apps
+void main() {
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('I am Rich'),
+            backgroundColor: Colors.blueGrey[900],
+      ),
+          body: Center(
+            child: Image(
+              image:
+                AssetImage('images/Logo.png'),
+        ),
+      ),
+    ),
+  )
+  );
+}
